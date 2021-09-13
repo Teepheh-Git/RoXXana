400bd09069c5a7c33cd394f513ef32199e97e5b1
Oguntoyinbo Boluwatife <81614588+Teepheh-Git@users.noreply.github.com>
2021-09-13 22:49:10 +0100

    Order Screen Done

--- screens/Order.js
+++ screens/Order.js
@@ -2,13 +2,15 @@ import React, {useEffect, useState} from 'react';
 import {
     View,
     Text,
+    Image,
     TouchableOpacity,
-    StyleSheet, SafeAreaView,
+    StyleSheet, SafeAreaView, FlatList,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {COLORS, dummyData, FONTS, icons, SIZES} from '../constants';
 import {IconButton, TabButtons, VerticalTextButton} from '../components';
 import Svg, {Circle} from 'react-native-svg';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
 const Order = ({navigation, route, appTheme}) => {
     const [selectedLocation, setSelectedLocation] = useState(null);
@@ -281,6 +283,94 @@ const Order = ({navigation, route, appTheme}) => {
                     {renderSideBar()}
 
                     {/*Listing*/}
+
+                    <FlatList
+                        contentContainerStyle={{
+                            marginTop: SIZES.padding,
+                            paddingBottom: 50,
+                        }}
+                        data={menu}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({item, index}) => {
+
+                            return (
+
+                                <TouchableWithoutFeedback
+                                    onPress={() => navigation.navigate('OrderDetail', {selectedItem: item})}>
+                                    <View style={{
+
+                                        height: 150,
+                                        paddingHorizontal: SIZES.padding,
+                                        marginTop: index > 0 ? SIZES.padding : 0,
+                                        alignItems: 'flex-end',
+                                        justifyContent: 'flex-end',
+
+                                    }}>
+
+                                        {/*Thumbnail*/}
+                                        <View style={{
+                                            position: 'absolute',
+                                            top: 0,
+                                            left: SIZES.padding,
+                                            width: 130,
+                                            height: 140,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderRadius: SIZES.radius,
+                                            backgroundColor: COLORS.lightYellow,
+                                            zIndex: 1,
+                                        }}>
+
+                                            <Image source={item.thumbnail}
+                                                   resizeMode={'contain'}
+                                                   style={{
+                                                       width: 100,
+                                                       height: 100,
+                                                   }}/>
+
+
+                                        </View>
+
+                                        {/*Details*/}
+
+                                        <View style={{
+                                            width: '70%',
+                                            height: '85%',
+                                            paddingLeft: '22%',
+                                            paddingRight: SIZES.base,
+                                            borderRadius: SIZES.radius,
+                                            justifyContent: 'space-between',
+                                            backgroundColor: COLORS.primary,
+                                        }}>
+
+                                            <Text style={{
+                                                color: COLORS.white,
+                                                ...FONTS.h2,
+                                                fontSize: 18,
+                                                lineHeight: 25,
+                                            }}>{item.name}</Text>
+
+
+                                            <Text style={{
+                                                color: COLORS.lightYellow,
+                                                ...FONTS.h2,
+                                                fontSize: 18,
+                                            }}>{item.price}</Text>
+
+
+                                        </View>
+
+                                    </View>
+
+
+                                </TouchableWithoutFeedback>
+
+
+                            );
+
+                        }}
+
+                    />
 
 
                 </View>
